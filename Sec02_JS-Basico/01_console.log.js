34bed82b1f1f01d2d312f35800fc9a3f0af6cfed
MarciaGSLeao <gameleira.mag@hotmail.com>
2022-02-08 23:16:16 -0300

    .

---
+++ Sec02_JS-Basico/01_console.log.js
@@ -0,0 +1,9 @@
+console.log("teste");
+console.log(12345);
+console.log(12.345);
+console.log(true);
+
+var nome = 'Marcia';
+console.log(nome);
+
+console.log('Olá, meu nome é "Márcia", e estou aprendendo JavaScript às', 18,'h da noite.');
