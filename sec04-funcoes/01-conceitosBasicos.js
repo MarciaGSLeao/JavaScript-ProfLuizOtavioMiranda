4f8ce44d061f1deea09d41bc2edaedd87b2501d5
Marcia Gameleira <gameleira.mag@hotmail.com>
2022-09-28 20:07:31 -0300

    .

--- sec04-funcoes/01-conceitosBasicos.js
+++ sec04-funcoes/01-conceitosBasicos.js
@@ -7,12 +7,31 @@ sayHello();
 function sayHello(){
     console.log(`Hello!`);
 }
-
 sayHello();
-
 
 // Uma função também pode ser atribuída a uma variável ou constante e portanto ser tratada como um dado.
 const sayGoodMorning = function() {
     console.log("Good Morning!");
 }
 sayGoodMorning();
+
+// Uma função pode receber outra função como parâmetro.
+function executaFuncao(funcao) {
+    console.log("Vou executar a função abaixo: ")
+    funcao();
+}
+executaFuncao(sayGoodMorning);
+
+// Arrow function. O conceito de arrow function é similar ao que chama-se como expressões lambdas no Java.
+const arrowFunction = () => {
+    console.log("Hello! Eu sou uma 'arrow function'");
+}
+arrowFunction();
+
+// Uma função também pode ser criada dentro de um objeto, como se fosse um método do objeto.
+const pessoa = {
+    habilidade: function(){
+        console.log("Falar......")
+    }
+}
+pessoa.habilidade();
